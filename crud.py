f67114dcc16fa070bf696e38a46dd3916fe14f41
Brittney Huttner <brittneyhuttner@gmail.com>
2020-05-29 19:25:40 +0000

    Add CRUD for utility functions

---
+++ crud.py
@@ -0,0 +1,19 @@
+"""CRUD operations."""
+
+from model import db, User, Movie, Rating, connect_to_db
+
+#Functions start here
+
+if __name__=='__main__':
+    from server import app
+    connect_to_db(app)
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user= User(email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
